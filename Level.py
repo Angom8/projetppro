981a656e9c463646500d8e16e47b9ce35d83bbfc
ClementA <49201096+ClementA@users.noreply.github.com>
2019-04-29 15:16:46 +0200

    Add files via upload

---
+++ Level.py
@@ -0,0 +1,27 @@
+
+class Level(object):
+	"""docstring for Level"""
+	def __init__(self, nom, rang1, rang2, rang3):
+		self.nom = nom
+		self.rang1 = rang1
+		self.rang2 = rang2
+		self.rang3 = rang3
+		self.position = []
+
+
+	"""getters"""
+	def getRang(self, rang):
+		if rang == 1:
+			return self.rang1
+		elif rang == 2:
+			return self.rang2
+		else:
+			return self.rang3
+
+
+	def getNom(self):
+		return self.nom
+
+
+	def getPosition(self):
+		return self.position
