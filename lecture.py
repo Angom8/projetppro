70817158ff2b38ebfbdbc4da4ae3a2878e7f599f
ClementA <49201096+ClementA@users.noreply.github.com>
2019-05-01 10:59:19 +0200

    Add files via upload

--- lecture.py
+++ lecture.py
@@ -6,12 +6,11 @@ def parcourir(tab, nb, String):
 	return nb
 
 def incrementer(nb1, nb2):
-	nb2 = nb1
 	nb1 += 1
+	nb2 = nb1
 
 
-from Level import Level
-
+import Level
 def lecture(nomLvl):
 	nom = nomLvl
 
@@ -22,24 +21,27 @@ def lecture(nomLvl):
 	virgule = ';'
 	
 	
-	compte = 0							#debut a 0
-	compte = parcourir(contenu, compte, barre)			#premiere barre (apres nom)
+	compte = 0										#debut a 0
+	parcourir(contenu, compte, barre)				#premiere barre (apres nom)
 	nom = contenu[4 : compte]
 
-	compte1 = compte + 6						#on saute le nom
-	compte = parcourir(contenu, compte, virgule)			
-	rang1 = str(contenu[compte1 : compte])				
+	compte = compte + 6								#on saute le nom
+	compte1 = compte
+	parcourir(contenu, compte, virgule)
+	rang1 = str(contenu[compte1 : compte])
 	rang1 = int(rang1)
 	
 	incrementer(compte, compte1)
-	compte = parcourir(contenu, compte, virgule)
+	parcourir(contenu, compte, virgule)
 	rang2 = str(contenu[compte1 : compte])
 	rang2 = int(rang2)
 	
 	incrementer(compte, compte1)
-	compte = parcourir(contenu, compte, virgule)
+	parcourir(contenu, compte, virgule)
 	rang3 = str(contenu[compte1 : compte])
-	rang3 = int(rang3)						#fin des rangs
+	rang3 = int(rang3)								#fin des rangs
+	
+	
 	
 	incrementer(compte, compte1)					#saut point virgule
 	compte = parcourir(contenu, compte, barre)
@@ -48,25 +50,24 @@ def lecture(nomLvl):
 	ligne[0]= rang.split(',')
 	
 	i = 0
-	while compte < len(contenu) and i < len(ligne):
+	while compte < len(contenu):
 		incrementer(compte, compte1)
-		compte = parcourir(contenu, compte, barre)
+		parcourir(contenu, compte, barre)
 		rang = contenu[compte1 : compte]
-		ligne[i]= rang.split(',')
+		ligne[i] = rang.spit(',')
 		i += 1
-		
-		
+
 	position = []
 	for x in range(i):
 		position.append(ligne[x])
-
-
+	
+	
+	
+	print str(rang1) + str(rang2) + str(rang3)
+	print str(position)
 
 
 	level = Level(nom, rang1, rang2, rang3)
-
-	print str(rang1) + str(rang2) + str(rang3)
-	print str(position)
 
 	level.setPosition(position)
 
