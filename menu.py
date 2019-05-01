5650c858b852b0d420e7ec59f9ca82a42945200b
Angom8 <angom8@outlook.fr>
2019-05-01 12:17:25 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,12 +1,16 @@
 import pygame
+from pygame.locals import *
 
 FPS = 30
 
 pygame.init()
-screen = pygame.display.set_mode((1000, 1000))
+screen = pygame.display.set_mode((640,480),HWSURFACE|DOUBLEBUF|RESIZABLE)
 
 pygame.display.set_caption("Place ton bloc ! - Menu")
+fond = pygame.image.load("img/background.jpg").convert()
+screen.blit(fond,(0,0))
 
+pygame.display.flip()
 clock = pygame.time.Clock()
 
 running = True
@@ -15,10 +19,13 @@ while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+		running = False
+        elif event.type==VIDEORESIZE:
+		screen=pygame.display.set_mode(event.dict['size'],HWSURFACE|DOUBLEBUF|RESIZABLE)
+		screen.blit(pygame.transform.scale(fond,event.dict['size']),(0,0))  
 
-    screen.fill((255,255,255))
-    pygame.display.flip()
     clock.tick(FPS)
-
+    pygame.display.flip()
+    print str(pygame.display.get_surface())
+    
 pygame.quit()
