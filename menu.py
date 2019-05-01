33fb4ce2a5a88755c8edcab6bfa30288ecda337c
Angom8 <angom8@outlook.fr>
2019-05-01 12:27:50 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -9,6 +9,7 @@ screen = pygame.display.set_mode((640,480),HWSURFACE|DOUBLEBUF|RESIZABLE)
 pygame.display.set_caption("Place ton bloc ! - Menu")
 fond = pygame.image.load("img/background.jpg").convert()
 screen.blit(fond,(0,0))
+pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(100,100, 200, 200))
 
 pygame.display.flip()
 clock = pygame.time.Clock()
@@ -22,7 +23,9 @@ while running:
 		running = False
         elif event.type==VIDEORESIZE:
 		screen=pygame.display.set_mode(event.dict['size'],HWSURFACE|DOUBLEBUF|RESIZABLE)
-		screen.blit(pygame.transform.scale(fond,event.dict['size']),(0,0))  
+		screen.blit(pygame.transform.scale(fond,event.dict['size']),(0,0))
+		largeur, hauteur = pygame.display.get_surface().get_size()
+		pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(100,100, largeur/2, hauteur/2))
 
     clock.tick(FPS)
     pygame.display.flip()
