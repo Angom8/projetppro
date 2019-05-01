0696a5acc0e03b5f922857cda4f65d723ccbf23e
Angom8 <angom8@outlook.fr>
2019-05-01 13:32:19 +0200

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,5 +1,15 @@
+
 import pygame
 from pygame.locals import *
+
+def normal_text(texte, x, y, color): 
+    font_text = pygame.font.SysFont('roboto', 17)
+    affichage = font_text.render(texte, 1, (color))
+    screen.blit(affichage,(x,y))
+def title_text(texte, x, y, color): 
+    font_text = pygame.font.SysFont('roboto', 35)
+    affichage = font_text.render(texte, 1, (color))
+    screen.blit(affichage,(x,y))
 
 FPS = 30
 
@@ -7,9 +17,12 @@ pygame.init()
 screen = pygame.display.set_mode((640,480),HWSURFACE|DOUBLEBUF|RESIZABLE)
 
 pygame.display.set_caption("Place ton bloc ! - Menu")
+pygame.display.set_icon(pygame.image.load("img/icone.png"))
+
 fond = pygame.image.load("img/background.jpg").convert()
 screen.blit(fond,(0,0))
-pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(100,100, 200, 200))
+largeur, hauteur = pygame.display.get_surface().get_size()
+pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(largeur/10,hauteur/10, 8*largeur/10, 8*hauteur/10))
 
 pygame.display.flip()
 clock = pygame.time.Clock()
@@ -24,11 +37,25 @@ while running:
         elif event.type==VIDEORESIZE:
 		screen=pygame.display.set_mode(event.dict['size'],HWSURFACE|DOUBLEBUF|RESIZABLE)
 		screen.blit(pygame.transform.scale(fond,event.dict['size']),(0,0))
-		largeur, hauteur = pygame.display.get_surface().get_size()
-		pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(100,100, largeur/2, hauteur/2))
-
+		
+    largeur, hauteur = pygame.display.get_surface().get_size()
+    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(largeur/10,hauteur/10, 8*largeur/10, 8*hauteur/10))	
+    pygame.font.init
+    title_text("Place ton bloc ! - Choisissez votre niveau :", largeur/7,hauteur/7, (0, 0, 0))
+    pygame.font.quit
+    
+    
+    
+    
+    
+    
+    
+    
+    
     clock.tick(FPS)
     pygame.display.flip()
-    print str(pygame.display.get_surface())
     
 pygame.quit()
+
+
+
