4e16fddfd14bd5e070c5af54e7bc131673189151
ClementA <49201096+ClementA@users.noreply.github.com>
2019-05-02 14:40:23 +0200

    Add files via upload

--- jeu.py
+++ jeu.py
@@ -1,4 +1,16 @@
 # -*- coding: iso-8859-15 -*-
 
-
-egrehtehtehethetheh
+def estTermine(tab, niveau):
+	i = 0
+	j = 0
+	verif = True
+	
+	while i < len(tab) and verif == True
+		while j < len(tab[i])
+			if tab[i][j] != niveau.getPosition()[i][j]
+				verif = False
+			else 
+				i += 1
+				j +=1
+	
+	return verif
