bccc1a1e042b74491de933137ced670a54c31667
Angom8 <angom8@outlook.fr>
2019-05-07 15:47:13 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -18,15 +18,17 @@ def fileremove(niveau):#uniquement custom
     os.remove("levels/" + niveau.getNom() + ".lvl")
     removeniveau(niveau)
   
-def affichageniveau(niveau, x, y):#le nom + bouton play
+def affichageniveau(niveau, x, y, screen):
     t, h = pygame.display.get_surface().get_size()
     normal_text(niveau.getNom(), x,y, (0, 0, 0))
     panel = pygame.draw.rect(screen, [64,164,151], pygame.Rect(int(x+ 2*t/3), y, 30, 15))
     normal_text("Play", x + int(2*t/3 + 5), y, (0, 0, 0))
+    rang = pygame.image.load("img/" + str(lecturesave(niveau)) + ".png").convert_alpha()
+    screen.blit(rang, (int(2*t/3 -30), y-5))
     retour = [niveau, panel, 1, 1]
     return(retour)
   
-def affichageniveaucustom(niveau, x, y):#le nom + boutons play, edit et delete
+def affichageniveaucustom(niveau, x, y, screen):
     t, h = pygame.display.get_surface().get_size()
     normal_text(niveau.getNom(), x,y, (0, 0, 0))
     suppr = pygame.draw.rect(screen, [255,0,0], pygame.Rect(int(x+ 2*t/3-10), y, 40, 15))
@@ -35,6 +37,8 @@ def affichageniveaucustom(niveau, x, y):#le nom + boutons play, edit et delete
     normal_text("Edit", x + int(2*t/3 + 5 - 40), y, (0, 0, 0))
     play = pygame.draw.rect(screen, [64,164,151], pygame.Rect(int(x+ 2*t/3 - 70), y, 30, 15))
     normal_text("Play", x + int(2*t/3 + 5 - 70), y, (0, 0, 0))
+    rang = pygame.image.load("img/" + str(lecturesave(niveau)) + ".png").convert_alpha()
+    screen.blit(rang, (int(2*t/3 -30), y-5))
     retour = [niveau, play, edit, suppr]
     return(retour)
     
@@ -68,7 +72,7 @@ while running:
 	
     largeur, hauteur = pygame.display.get_surface().get_size()
     
-    if largeur > 400 or hauteur > 400:#sinon affichage pas bon
+    if largeur > 400 or hauteur > 400:
 	fond = pygame.image.load("img/background.jpg").convert()
     	screen.blit(fond,(0,0))
    	menu = pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(largeur/10,hauteur/10, 8*largeur/10, 8*hauteur/10))
@@ -85,8 +89,10 @@ while running:
 		for c in range(len(niveau)):
 			collide = niveau[c][1].collidepoint(x, y)
 			if collide:
+
+       				## Détecte les clique de souris.
         			pressed = pygame.mouse.get_pressed()
-        			if pressed[0]:
+        			if pressed[0]: # 0=gauche, 1=milieu, 2=droite
             				print "Opening ... " + niveau[c][0].getNom()
 					jeu(niveau[c][0]) 
                 		
@@ -96,14 +102,18 @@ while running:
        			if niveau[c][2] != 1:
 				collide = niveau[c][2].collidepoint(x, y)
 				if collide:
+
+       					## Détecte les clique de souris.
         				pressed = pygame.mouse.get_pressed()
-        				if pressed[0]:
+        				if pressed[0]: # 0=gauche, 1=milieu, 2=droite
             					print "Editing ... " + niveau[c][0].getNom()
 						editeur(niveau[c][0])
 				collide = niveau[c][3].collidepoint(x, y)
 				if collide:
+
+       					## Détecte les clique de souris.
         				pressed = pygame.mouse.get_pressed()
-        				if pressed[0]:
+        				if pressed[0]: # 0=gauche, 1=milieu, 2=droite
             					print "Deleting ... " + niveau[c][0].getNom()
 						fileremove(niveau[c][0])
 			c+=1
@@ -115,11 +125,11 @@ while running:
     	normal_text("Niveaux standards :", largeur/7,hauteur/7 + 30, (0, 0, 0))
     
     
-    	niveau.append(affichageniveau(lecture("baselevels/Niveau1.lvl"), largeur/7, hauteur/7 + 60))
-    	niveau.append(affichageniveau(lecture("baselevels/Niveau2.lvl"), largeur/7, hauteur/7 + 80))
-    	niveau.append(affichageniveau(lecture("baselevels/Niveau3.lvl"), largeur/7, hauteur/7 + 100))
-	niveau.append(affichageniveau(lecture("baselevels/Niveau4.lvl"), largeur/7, hauteur/7 + 120))
-	niveau.append(affichageniveau(lecture("baselevels/Niveau5.lvl"), largeur/7, hauteur/7 + 140))
+    	niveau.append(affichageniveau(lecture("baselevels/Niveau1.lvl"), largeur/7, hauteur/7 + 60, screen))
+    	niveau.append(affichageniveau(lecture("baselevels/Niveau2.lvl"), largeur/7, hauteur/7 + 80, screen))
+    	niveau.append(affichageniveau(lecture("baselevels/Niveau3.lvl"), largeur/7, hauteur/7 + 100, screen))
+	niveau.append(affichageniveau(lecture("baselevels/Niveau4.lvl"), largeur/7, hauteur/7 + 120, screen))
+	niveau.append(affichageniveau(lecture("baselevels/Niveau5.lvl"), largeur/7, hauteur/7 + 140, screen))
     
     	normal_text("Niveaux standards :", largeur/7,hauteur/7 + 170, (0, 0, 0))
     
@@ -128,7 +138,7 @@ while running:
 	    
     	for d in range(len(liste)):
     		if str(liste[d][len(liste[d])-4:len(liste[d])]) == ".lvl":	
-    			niveau.append(affichageniveaucustom(lecture("levels/" + liste[d]), largeur/7, hauteur/7 + 200 + d*30))
+    			niveau.append(affichageniveaucustom(lecture("levels/" + liste[d]), largeur/7, hauteur/7 + 200 + d*30, screen))
     
     
     	pygame.font.quit
