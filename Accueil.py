fd35c7ffb85c0b65fca35e416f6229f26b286200
Angom8 <angom8@outlook.fr>
2019-05-08 15:17:17 +0200

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -1,4 +1,6 @@
+# -*- coding: iso-8859-15 -*-
 import pygame
+import os
 from pygame.locals import *
 from math import sin
 from menu import *
@@ -7,18 +9,18 @@ import time
 FPS = 30
 
 pygame.init()
-fenetre = pygame.display.set_mode((640, 480), HWSURFACE|DOUBLEBUF|RESIZABLE)
-fond = pygame.image.load("Images/background.jpg").convert()
+fenetre = pygame.display.set_mode((640, 480))
+fond = pygame.image.load("img/background.jpg").convert()
 fenetre.blit(fond, (0,0))
 
-pygame.display.set_caption("Démarrage")
+pygame.display.set_caption("Demarrage")
 
-logo = pygame.image.load("Images/URCA.png").convert_alpha()
+logo = pygame.image.load("img/URCA.png").convert_alpha()
 
-texte = "THEBAULT Antoine / ALEXANDRE Clément"
-font = pygame.font.SysFont('Consolas', 20)
+texte = "THEBAULT Antoine / ALEXANDRE Clement"
+font = pygame.font.SysFont('roboto', 20)
 
-pygame.mixer.music.load("Sons/intro.mp3")
+pygame.mixer.music.load("sons/intro.mp3")
 
 counter, text = 8, '180'.rjust(3)
 pygame.time.set_timer(pygame.USEREVENT, 1000)
@@ -28,29 +30,22 @@ clock = pygame.time.Clock()
 	
 running = True
 
-while running:
-	pygame.init()	
+while running:	
 	for event in pygame.event.get():
 		if event.type == pygame.USEREVENT: 
 			counter -= 1
 			text = str(counter).rjust(3) if counter > 0 else pygame.quit()
 		if event.type == pygame.QUIT:
 			running = False
-		elif event.type==VIDEORESIZE:
-			screen=pygame.display.set_mode(event.dict['size'],HWSURFACE|DOUBLEBUF|RESIZABLE)
-			screen.blit(pygame.transform.scale(fond,event.dict['size']),(0,0))
-			largeur, hauteur = pygame.display.get_surface().get_size()
-			pygame.mixer.music.play()
-			fenetre.blit(logo, ((largeur/2) - 65, hauteur*0.45))
-			pygame.display.flip()
-			pygame.time.wait(4000)
-			fenetre.blit(fond , (0,0))
-			fenetre.blit(font.render(texte, True, (0, 0, 0)), ((largeur/5) , hauteur*0.5))
-			pygame.display.flip()
-			pygame.time.wait(3000)
-			menu()
-			clock.tick(FPS)
-	
-
-pygame.mixer.music.stop()
-pygame.quit()
+	largeur, hauteur = pygame.display.get_surface().get_size()
+	pygame.mixer.music.play()
+	fenetre.blit(logo, ((largeur/2) - 65, hauteur*0.45))
+	pygame.display.flip()
+	pygame.time.wait(4000)
+	fenetre.blit(fond , (0,0))
+	fenetre.blit(font.render(texte, True, (0, 0, 0)), ((largeur/5+70) , hauteur*0.5))
+	pygame.display.flip()
+	pygame.time.wait(3000)
+	pygame.mixer.music.stop()
+	clock.tick(FPS)
+	menu()
