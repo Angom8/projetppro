58440288d92eef208d3ff1705e38f64bbb3b7ba7
ClementA <49201096+ClementA@users.noreply.github.com>
2019-05-08 11:07:53 +0200

    Add files via upload

---
+++ estTermine.py
@@ -0,0 +1,14 @@
+def estTermine(tab, niveau):
+	i = 0
+	j = 0
+	verif = True
+	
+	while i < len(tab) and verif == True
+		while j < len(tab[i])
+			if tab[i][j] != niveau.getPosition()[i][j]
+				verif = False
+			else 
+				i += 1
+				j +=1
+	
+	return verif
