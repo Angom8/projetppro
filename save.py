2af1337401c32450c858fa8e91b7b49ef88304b5
Angom8 <angom8@outlook.fr>
2019-05-08 11:35:20 +0200

    Update save.py

--- save.py
+++ save.py
@@ -19,35 +19,38 @@ def savelevel(niveau):
 	save.close()
 
 def addsave(niveau, rang):
-	add = open("saves/save.sv", "w")
-	
-	contenu = lecture.read()
-
-	contenu[len(contenu)-1] = null
-	
-	add = open("saves/save.sv", "a")
-	
-	add.write(str(contenu[0:(len(contenu)-1)])
-	
-	add.write("," + niveau.getNom() + ":" + str(rang) + "#")
+        add = open("saves/save.sv", "r")
+        contenu = add.read()
+        contenu.remove(len(contenu)-1)
+        add.close()
+        ad = open("saves/save.sv", "a")
+        ad.write(str(contenu[0:(len(contenu)-1)] + str("," + niveau.getNom() + ":" + str(rang) + "#")))
+        ad.close()
 
 def removesave(niveau):
-	efface = open("saves/save.sv", "w")
+	efface = open("saves/save.sv", "r")
 	
-	contenu = lecture.read()
+	raw = efface.read()
+	contenu = [i for i in raw]
 	debut = 0
-	fin = 4
+	fin = 0
 	
-	while str(contenu[debut:fin]) != niveau.getNom() and tab[fin] != "#":#tant que on ne l'a pas trouve ou que nous ne somme pas a la fin du fichier
-		debut = fin
-		while contenu[fin] != "#" and contenu[fin] != ",":
+	while str(contenu[debut+1:(fin)]) != niveau.getNom() and str(contenu[fin]) != "#":#tant que on ne l'a pas trouve ou que nous ne somme pas a la fin du fichier
+		if fin != 0:
+			debut = fin + 2
+		fin += 1
+		while str(contenu[fin]) != "#" and str(contenu[fin]) != ":":
 			fin += 1
 	
 	if contenu[fin] != "#":
 		while contenu[debut] != "," or contenu[debut] != "#":
 			contenu.remove(debut)
+		
+	contenu.remove(len(contenu)-1)
 			
-			
-	efface.close
-	efface = open("saves/save.sv", "a")
-	efface.write(str(contenu))
+	efface.close()
+	efface = open("saves/save.sv", "w")
+	for i in range(len(contenu)):
+		efface.write(str(contenu[i]))
+	efface.write("#")
+	efface.close()
