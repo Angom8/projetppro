95f8e3bdf4136796f634147642117dfccd922f56
Angom8 <angom8@outlook.fr>
2019-05-09 14:38:43 +0200

    VFinale : Ajout de la doc

--- Level.py
+++ Level.py
@@ -1,7 +1,7 @@
 # -*- coding: iso-8859-15 -*-
 
 class Level(object):
-	"""docstring for Level"""
+	#Chaque niveau possede un nom et 3 entiers definissants les temps max en secondes pour les rangs 1 2 et 3
 	def __init__(self, nom, rang1, rang2, rang3):
 		self.nom = nom
 		self.rang1 = rang1
@@ -10,7 +10,7 @@ class Level(object):
 		self.position = []
 
 
-	"""getters"""
+	#Getter de Rang. Retourne la valeur en seconde en specifiant le rang voulu.
 	def getRang(self, rang):
 		if rang == 1:
 			return self.rang1
@@ -19,19 +19,19 @@ class Level(object):
 		else:
 			return self.rang3
 
-
+	#Getter du Nom
 	def getNom(self):
 		return self.nom
 
-
+	#Getter du tableau de positions des blocs
 	def getPosition(self):
 		return self.position
 
-
+	#Getter setPosition pour la creation de niveau
 	def setPosition(self, tab) :
 		self.position = tab
 	
-	
+	#Pas utile. Permet d'afficher la description du niveau
 	def _str_(self):
 		result = 'Niveau :' + self.nom + 'de rang 1 : ' + self.rang1 + ', rang 2 : ' + self.rang2 + ' et de rang 3 : ' + self.rang3
 		return result
