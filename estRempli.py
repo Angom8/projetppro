9d784059c28b2c0556a1146e5c384616ddc05ee4
ClementA <49201096+ClementA@users.noreply.github.com>
2019-05-10 17:19:27 +0200

    Add files via upload

---
+++ estRempli.py
@@ -0,0 +1,14 @@
+def estRempli(taille):
+	termine = False
+	
+	for c in range(len(taille)):
+		for l in range(len(taille[c]):
+			if taille[][] == 0:
+				termine = False
+			else:
+				termine = True
+				
+	if termine == True:
+		savelevel(taille)
+	
+	return(termine)
