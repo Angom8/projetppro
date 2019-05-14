64a15a30cbe4d12075d236b6d6c41c22d55e8db9
ClementA <49201096+ClementA@users.noreply.github.com>
2019-05-14 09:55:03 +0200

    Add files via upload

---
+++ music.py
@@ -0,0 +1,23 @@
+#menu
+	#à placer dans l'intialisation
+		pygame.mixer.music.load("sons/menu.mp3")
+	#à placer dans la boucle d'événement
+		pygame.mixer.music.play()
+	#fin son
+		pygame.mixer.music.stop()
+		
+#jeu
+	#à placer dans l'intialisation
+		pygame.mixer.music.load("sons/jeu.mp3")
+	#à placer dans la boucle d'événement
+		pygame.mixer.music.play()
+	#fin son
+		pygame.mixer.music.stop()
+
+#editeur
+	#à placer dans l'intialisation
+		pygame.mixer.music.load("sons/editeur.mp3")
+	#à placer dans la boucle d'événement
+		pygame.mixer.music.play()
+	#fin son
+		pygame.mixer.music.stop()
